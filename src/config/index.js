175a809c5c4d6a8f47ec254beb009151be55572f
SakibHasanCse <sakibhasan9641@gmail.com>
2021-10-02 23:58:27 +0600

    added file stutuctures

---
+++ src/config/index.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+
+//load .env file
+dotenv.config();
+
+const config = {
+    http_port: process.env.PORT || 8080,
+    cors_origin: process.env.CORS_REGEX
+        ? [new RegExp(process.env.CORS_REGEX)]
+        : [/localhost/],
+    mongo: {
+        url: process.env.DB_URL || 'mongodb://localhost:27017',
+        db: process.env.DB_NAME || 'graphql_Ecommers',
+        reconnectedTimeout: process.env.DB_RECONNECT_TIME || 5000
+    },
+    public_key_file: process.env.JWT_PUB_KEY_FILE || path.join(__dirname, '../sample_keys/public_key.pem'),
+    public_key_b64: process.env.JWT_PUB_KEY_B64,
+    logging_level: process.env.LOGGING_LEVEL || (process.env.NODE_ENV === 'production' ? 'info' : 'debug'),
+    db_logging_log: process.env.DB_DEBUG_LOG || process.env.NODE_ENV !== 'production'
+}
+
+export default config;
+
+export const jwtpublickey = (() => {
+    if (config.public_key_b64) {
+        return new Buffer(config.public_key_b64, 'base64').toString('ascii')
+    }
+    return fs.readFileSync(config.public_key_file);
+})();
--- src/server.js
+++ src/server.js
@@ -1,34 +1,32 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
+import cookiesParser from 'cookies-parser';
+import mongoose from 'mongoose';
 
-import { ApolloServer } from 'apollo-server-express';
-import db from './data';
-import resolvers from './graphql/resolvers';
 
-const pubsub = new PubSub();
-const server = new ApolloServer({
-    typeDefs: './src/graphql/schema/typeDefs.graphql',
-    resolvers,
-    introspection: true,
-    uploads: false,
-    context: {
-        data,
-        pubsub
-    },
-    formatErrors: (err) => {
-        if (err.message.startsWith('Database Error: ')) {
-            return new Error('Internal server error');
-        }
-        // Otherwise return the original error. The error can also
-        // be manipulated in other ways, as long as it's returned.
-        return err;
-    },
-    playground: {
-        settings: {
-            'editor.theme': 'light'
-        }
-    }
-});
+import config from './config';
+import Schema from './schema';
 
-const port = process.env.PORT || 3000;
-server.start(({ port }), function () {
+const app = express();
+
+app.use(cors({
+    origin: config.cors.origin,
+    credentials: true
+}))
+
+app.use(cookiesParser());
+
+
+const graphqlHttpOptions = {
+    graphql: true,
+    pretty: true,
+    schema: Schema
+}
+
+app.use('/graphql', graphqlHTTP((request, response, graphQLParams) => ({ ...graphqlHttpOptions, context: request.gqlviewer })))
+
+
+app.listen(3000, function () {
     console.log(`Server listening on port ${port}`);
 });
