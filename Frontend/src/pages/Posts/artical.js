c44441b6ec0eec5ab41bc04f68e457e4160952c0
SakibHasanCse <sakibhasan9641@gmail.com>
2021-11-21 22:30:53 +0600

    Added prettier config

--- Frontend/src/pages/Posts/artical.js
+++ Frontend/src/pages/Posts/artical.js
@@ -2,7 +2,7 @@ import { useLazyQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react';
 import { ArrowRight } from 'react-feather';
 import { GET_ALL_POSTS } from './graphql/getAllPosts.graphql'
-
+import { Link } from 'react-router-dom';
 let demodata = [{
     _id: '1',
     title: 'Hello title ',
@@ -60,13 +60,13 @@ const Artical = () => {
                         <img src={item.image ? item.image : 'https://source.unsplash.com/collection/1346951/1000x500?sig=1'} alt={item.title} />
                     </a>
                     <div className="flex flex-col justify-start p-6">
-                        <a href={`/${item.slug}`} className="text-blue-700 text-sm font-bold uppercase pb-4">{item.category}</a>
-                        <a href={`/${item.slug}`} className="text-3xl font-bold hover:text-gray-700 pb-4">{item.title}</a>
+                        <Link to={`/${item.slug}`} className="text-blue-700 text-sm font-bold uppercase pb-4">{item.category}</Link>
+                        <Link to={`/${item.slug}`} href={`/${item.slug}`} className="text-3xl font-bold hover:text-gray-700 pb-4">{item.title}</Link>
                         <p className="text-sm pb-3">
                             By <a href="/">{item.author && item.author.name || ''}</a>, Published on Aprill 2021
                             </p>
                         <span className="pb-6">{item.description || ''}</span>
-                        <a href={`/${item.slug}`} className="flex uppercase text-gray-800 hover:text-black">Continue Reading <ArrowRight /></a>
+                        <Link to={`/${item.slug}`} className="flex uppercase text-gray-800 hover:text-black">Continue Reading <ArrowRight /></Link>
                     </div>
                 </artical>
             ))}
